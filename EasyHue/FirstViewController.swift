1854a6846c28cfc2a856dc04af3b978ae9b5a2d2
Jochen Pfeiffer <jochen.pfeiffer@mobileeventguide.de>
2016-02-23 15:02:10 +0100

    initial commit

---
+++ EasyHue/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  EasyHue
+//
+//  Created by Jochen Pfeiffer on 23.02.16.
+//  Copyright © 2016 Jochen Pfeiffer. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
