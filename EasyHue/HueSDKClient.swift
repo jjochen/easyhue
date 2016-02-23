d9ff982a84793d3d3a91715ea1b3d5d913a37e14
Jochen Pfeiffer <jochen.pfeiffer@mobileeventguide.de>
2016-02-23 20:49:18 +0100

    started Hue SDK client

---
+++ EasyHue/HueSDKClient.swift
@@ -0,0 +1,19 @@
+//
+//  HueClient.swift
+//  EasyHue
+//
+//  Created by Jochen Pfeiffer on 23.02.16.
+//  Copyright © 2016 Jochen Pfeiffer. All rights reserved.
+//
+
+import Foundation
+
+
+class HueSDKClient: NSObject {
+    
+    lazy var sdk = PHHueSDK()
+    
+    
+    
+}
+
